e8f01100953b882f8fc2596b62a61bcf59d6d38d
Andres Lee <andreslee@dominiomundo.com>
2016-07-28 13:39:56 -0500

    Update putler class.

--- Putler.php
+++ Putler.php
@@ -6,17 +6,18 @@
  */
 class Putler
 {
-	private function currencyExchange($currency) {
-		$string = file_get_contents('http://www.getexchangerates.com/api/latest.json');
-		$json_a = json_decode($string, true);
+	public function init()
+    {
+        return;
+    }
 
-		foreach ($json_a as $person_name => $person_a) {
-		    return $person_a[$currency];
-		}
-		
+	private function currencyExchange($currency) {
+		$string = file_get_contents('https://www.apilayer.net/api/live?access_key=075bf6009fe4355529edc0a9edf9fe7f');
+		$json_a = json_decode($string, true);
+		return $json_a['quotes'][$currency];
 	}
 
-	private function putlerValidations() {
+	public function putlerValidations() {
 		$output = Yii::app()->curl->setOption(CURLOPT_USERPWD, "" . Yii::app()->params['putler']['email'] . ":" . Yii::app()->params['putler']['token'] . "")->post(Yii::app()->params['putler']['url'], $data, array('action' => 'validate'));
 		return json_decode($output);
 	}
@@ -39,14 +40,14 @@ class Putler
 				$timezone = 'GMT';
 				$type 	= "Shopping Cart Payment Received";
 				$transaction_id = $cartOrder->gateways_transaction_id;
-				$item_title = "Order: " . $cartOrder->id . "";
+				$item_title = "Discovery Mundo - Order: " . $cartOrder->id . "";
 				$quantity = 1;
 				$source = "Credit Card";
 				$name 	= $cartClient->name;
 				$status = "Completed";
 				$currency = "USD";
 				$gross 	= $cartOrder->amount_total;
-				$fee   	= round((($transaction_fee/100)/$this->currencyExchange('MXN')), 2);
+				$fee   	= round((($transaction_fee/100)/$this->currencyExchange('USDMXN')), 2);
 				$net   	= round($gross - $fee, 2);
 				$email  = $cartClient->email;
 				$item_id = $cartOrder->id;
@@ -57,16 +58,16 @@ class Putler
 
 				$json = '[{"Date":"' . $date . '","Time":"' . $time . '","Time_Zone":"' . $timezone . '","Type":"' . $type . '","Transaction_ID":"' . $transaction_id . '","Item_Title":"' . $item_title . '","Quantity":' . $quantity . ',"Source":"' . $source . '","Name":"' . $name . '","Status":"' . $status . '","Currency":"' . $currency . '","Gross":' . $gross . ',"Fee":-' . $fee . ',"Net":' . $net . ',"From_Email_Address":"' . $email . '","Item_ID":"' . $item_id . '","Address_Line_1":"' . $address . '","Address_Line_2":"' . $address2 . '","Town_City":"' . $city . '","Contact_Phone_Number":"' . $phone . '"}]';
 
-				$ch = curl_init(Yii::app()->params['putler']['url']);                                                                      
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); 
-				curl_setopt($ch, CURLOPT_USERPWD, "" . Yii::app()->params['putler']['email'] . ":" . Yii::app()->params['putler']['token'] . "");                                                                    
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $json);                                                                  
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-				curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-				    'Content-Type: application/json',                                                                           
-				    'Content-Length: ' . strlen($json))                                                                       
-				);                                                                                                                   
-				 
+				$ch = curl_init(Yii::app()->params['putler']['url']);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_USERPWD, "" . Yii::app()->params['putler']['email'] . ":" . Yii::app()->params['putler']['token'] . "");
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+					'Content-Type: application/json',
+					'Content-Length: ' . strlen($json))
+				);
+
 				$result = curl_exec($ch);
 			}
 		}
